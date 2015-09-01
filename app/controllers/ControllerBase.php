031fb4a37681031b0b3c25f6bfa233de94f6845e
mm-maruyama <knoveasel@gmail.com>
2015-09-01 16:22:19 +0900

    test

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -5,4 +5,12 @@ use Phalcon\Mvc\Controller;
 class ControllerBase extends Controller
 {
 
+    public function beforeExecuteRoute($dispatcher)
+    {
+		// セッションデータの設定
+		$this->session->set('auth', array(
+		  'id' => 123,
+		  'name' => 'テスト'
+		));
+	}
 }
